93a18a8cb3fb68ca3311b6bb8f07cc496bba5f4e
Gerald Hewes <gerald@geraldthewes.org>
2014-06-15 11:22:00 -0400

    Cleanup license, fix tests

--- tests/testthat/testdataframe.R
+++ tests/testthat/testdataframe.R
@@ -9,18 +9,49 @@ test_that("Dataframe Basics", {
     expect_that(x$ncol(), equals(29))
 
     expect_that(x$names()[3], equals("DayOfMonth"))
+    expect_that(x$names()[19], equals("Distance"))
 
+    expect_that(length(x$partitions()), equals(1))
     expect_that(x$partitions()[1], equals("2008-01-03"))
 
     expect_that(x$partition_rows('2008-01-03'), equals(999))
+    expect_that(x$partition_splits('2008-01-03'), equals(2))
 
-    v <- x$split('2008-01-03',1,1)
+    # Test integer vectods
+    v <- x$split('2008-01-03',1,19)
 
     expect_that(length(v), equals(500))
+    expect_that(sum(v), equals(323591))
+    expect_that(v[100], equals(1987))
     
-    v <- x$split('2008-01-03',2,1)
-
+    v <- x$split('2008-01-03',2,19)
     expect_that(length(v), equals(499))
+})
+
+
+test_that("Dataset Iris", {
+    
+    scy <- Module("rscythica", PACKAGE="RScythica")
+    sdf2 <- scy$SDataframe
+    x <- new(sdf2,path="/home3/gth/go/src/github.com/geraldthewes/scythica/sdsmeta/iris")
+
+    expect_that(x$ncol(), equals(5))
+
+    expect_that(x$names()[3], equals("Petal.Length"))
+
+    expect_that(length(x$partitions()), equals(3))
+    expect_that(x$partitions()[1], equals("Iris-setosa"))
+
+    expect_that(x$partition_rows('Iris-setosa'), equals(50))
+    expect_that(x$partition_splits('Iris-setosa'), equals(1))
+
+    # test double vectors
+    v <- x$split('Iris-setosa',1,1)
+
+    expect_that(length(v), equals(50))
+    expect_that(sum(v), equals(250.3))
+    expect_that(v[25], equals(4.8))
+    
 })
 
 
