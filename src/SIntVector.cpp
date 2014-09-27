f7f84001a363e5164904ec30a11889c5646f305a
Gerald Hewes <gerald@geraldthewes.org>
2014-09-27 18:54:08 -0400

    Use a raw vector insteal of integer

--- src/SIntVector.cpp
+++ src/SIntVector.cpp
@@ -28,44 +28,42 @@ SIntVector::SIntVector() {
 }
 
 SEXP SIntVector::select_op_eq(SEXP v, SEXP out, int value) {
-    int *varray = INTEGER(v);
-    int l = LENGTH(v);
+    __m128i *vptr128i = (__m128i *)INTEGER(v);
+    int l32 = LENGTH(v);
     
     // Output bitmap needs to be right size
-    uint64_t *vout = (uint64_t *)INTEGER(out);
-    int vl = LENGTH(out);
+    uint8_t *vout8 = (uint8_t *)RAW(out);
+    int vl8 = LENGTH(out);
+        
+    // Check that array is correctly 16byte aligned for MMX
+    if ((intptr_t)vptr128i % 16 != 0) return R_NilValue;
     
-    // Needs to be even size since using 64bit vectors
-    if (vl % 2 != 0) return R_NilValue;
-    if ((intptr_t)varray % 16 != 0) return R_NilValue;
-    if ((intptr_t)vout % 2 != 0) return R_NilValue;
+    // Check that out array is of right length
+    //if (vl32 != (l32+3)/4) return R_NilValue;
+    if (vl8 != l32) return R_NilValue;
     
-    int waves = l/64;
-    if (waves != vl/2) return R_NilValue;
+    int waves = l32/4;
     
     __m128i value4 = _mm_set1_epi32(value);
     
-    for(int w=0;w<waves;w++) {
-      // Load vector 64 Ints (16 x 4 ints)
-      // Assumption is int = 4byte
-      __m128i *vbase = (__m128i *)(varray + w*16);
-      uint64_t bit64 = 0;
-      for (int i=0;i<16;i++) {
-        __m128i v4 = _mm_loadu_si128(vbase + i);
-        __m128i o4 = _mm_cmpeq_epi32(v4, value4);
-    
-        uint64_t bit4 =  _mm_extract_epi32(o4,0)?0x01:0x00 |
-                    _mm_extract_epi32(o4,1)?0x02:0x00 |
-                    _mm_extract_epi32(o4,2)?0x04:0x00 |
-                    _mm_extract_epi32(o4,3)?0x08:0x00;
-        bit4 <<= i*4;
-        bit64 |= bit4;
-      }
-      vout[w] = bit64;
+    for(int w=0;w<waves;w++, vptr128i++) {
+      // Load vector 4 Ints 
       
+      __m128i v4 = _mm_load_si128(vptr128i);
+      __m128i o4 = _mm_cmpeq_epi32(v4, value4);
+      
+      *vout8++ = (uint8_t)_mm_extract_epi32(o4,0);
+      *vout8++ = (uint8_t)_mm_extract_epi32(o4,1);
+      *vout8++ = (uint8_t)_mm_extract_epi32(o4,2);
+      *vout8++ = (uint8_t)_mm_extract_epi32(o4,3);   
     }
     
     // Deal with the last partial wave
+    int *tail32 = (int*)vptr128i;
+    int iter = l32%4;
+    for(int i=0; i < iter; i++) {
+        *vout8 ++ = (*tail32++ == value) ? 0xff : 0x00;   
+    }
     
     return out;
 }
