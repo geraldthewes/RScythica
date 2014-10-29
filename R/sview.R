3518fbfb2b5ba06447a6acdcc404d6603f86d953
Gerald Hewes <gerald@geraldthewes.org>
2014-10-28 23:01:01 -0400

    Implemented view collapse

--- R/sview.R
+++ R/sview.R
@@ -21,7 +21,9 @@ v  = list(ds=ds,
           partitions=NULL, 
           columns=NULL,
           filter=NULL,
-          bitmap=NULL)
+          bitmap=NULL,
+          klen=NULL,
+          rows=0)
 class(v) <- "sview"
 v
 }
@@ -136,6 +138,7 @@ sview_execute_filter <- function(v) {
   m   <- Module("rscythica", PACKAGE="RScythica")
   bm <- m$BitVector
   v$bitmap <- list()
+  rows <- 0
   for (i in 1:nrow(v$partitions)) {
     splits <- (v$partitions)[i,"splits"]
     p <- as.character((v$partition)[i,"partition"])
@@ -144,13 +147,17 @@ sview_execute_filter <- function(v) {
       sindex <- sindex(srows)
       vfactory <- m$SIntVector
       bm.v <- new(vfactory)
-      a <- bm.v$op.gt((v$ds)$splitn(p,s,(v$filter)$var) ,
+      b <- bm.v$op.gt((v$ds)$splitn(p,s,(v$filter)$var) ,
                       sindex,
                       (v$filter)$val)
       k <- sdataframe_key(p,s)
-      v$bitmap[[k]] <- a
+      v$bitmap[[k]] <- b
+      bv <- new(bm,b)
+      v$klen[[k]] <-  bv$popcount()
+      rows <- rows + v$klen[[k]]
     }
   }
+  v$rows <- rows
   v
 }
 
@@ -162,18 +169,7 @@ sview_execute_filter <- function(v) {
 #'  frow <- sview_rows(views)
 #' @export 
 sview_rows <- function(v) {
-  rows = 0;
-  
-  if (!is.null(v$bitmap)) {
-    m   <- Module("rscythica", PACKAGE="RScythica")
-    bm <- m$BitVector
-    for(b in v$bitmap) {
-      bv <- new(bm,b)
-      rows <- rows + bv$popcount()
-    }
-  }
-  
-  rows
+  v$rows
 }
 
 #' Execute query
@@ -184,16 +180,15 @@ sview_rows <- function(v) {
 #'  view <- sview(views, "2008-01-03")
 #' @export 
 sview_execute <- function(v) {
-  rows = sview_rawrows(v)
   types <- (v$ds)$col_types()
   res <- NULL
   
   for (c in v$columns) {
     col <- switch(types[c],
-             int32=integer(rows),
-             factor=factor(rows),
-             logical=logical(rows),
-             integer(rows))
+             int32=integer(v$rows),
+             factor=factor(v$rows),
+             logical=logical(v$rows),
+             integer(v$rows))
    
     if (is.null(res)) {
       res <- data.frame(col)
@@ -205,14 +200,22 @@ sview_execute <- function(v) {
   }
   
   rows = 1L;
-  rows_per_split <- (v$ds)$rows_per_split
-  for (p in v$partitions) {
-    splits <- (v$ds)$partition_splits(p)
+  #rows_per_split <- (v$ds)$rows_per_split
+  m   <- Module("rscythica", PACKAGE="RScythica")
+  integer.vec <- new(m$SIntVector)
+  for (i in 1:nrow(v$partitions)) {
+    splits <- (v$partitions)[i,"splits"]
+    p <- as.character((v$partition)[i,"partition"])
     for (s in 1:splits) {  
-      srows <-  if (s == splits) (v$ds)$partition_rows(p) %% rows_per_split else rows_per_split
-      nrows  <- rows + srows - 1L
+      k <- sdataframe_key(p,s)
+      nrows  <- rows + v$klen[[k]] - 1L
       for (c in v$columns) {
-        res[[c]][rows:nrows] <- (v$ds)$splitn(p,s,c) 
+        col <- switch(types[c],
+                      int32=integer.vec$collapse((v$ds)$splitn(p,s,c),(v$bitmap)[[k]]),
+                      factor=NULL,
+                      logical=NULL,
+                      NULL)      
+        res[[c]][rows:nrows] <- col 
       }
       rows <- nrows + 1L
     }
