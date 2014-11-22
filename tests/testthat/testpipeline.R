2f6b50b429c3051de81cc089ce65cc26f9279bea
Gerald Hewes <gerald@geraldthewes.org>
2014-11-22 13:30:24 -0500

    Added new tests

---
+++ tests/testthat/testpipeline.R
@@ -0,0 +1,56 @@
+context("Scythica Dataframe Pipeline")
+
+
+
+test_that("Test Basic Pipeline", {
+  sdf <- open_sdataset("../extdata/airline.sds")
+  l <- 5
+  columns <- c('Distance','TaxiIn')
+ 
+  v <- sview(sdf)
+  v <- sv_partitions(v,c("2008-01-03"))
+
+  v <- sv_subset(v,columns)
+  v <- sv_filter(v,Distance > 2000 & TaxiIn < l)
+
+  ds2 <- sv_execute(v)
+  
+  expect_that(nrow(ds2), testthat::equals(10))  
+  expect_that(sv_rows(v), testthat::equals(10))  
+  expect_that(ncol(ds2), testthat::equals(2)) 
+  expect_that(names(ds2),testthat::equals(columns))
+  
+  expect_that(sum(ds2$Distance), testthat::equals(22265))
+  expect_that(min(ds2$Distance), testthat::equals(2027))
+  
+  expect_that(sum(ds2$TaxiIn), testthat::equals(35))
+  expect_that(max(ds2$TaxiIn), testthat::equals(4))
+
+})
+
+test_that("Test Basic Pipeline - 2 ", {
+  sdf <- open_sdataset("../extdata/airline.sds")
+  l <- 5
+  columns <- c('Distance','TaxiIn')
+  
+  v <- sview(sdf)
+  v <- sv_partitions(v,c("2008-01-03"))
+  
+  v <- sv_subset(v,columns)
+  v <- sv_filter(v,l > TaxiIn  & 2000 < Distance)
+  
+  ds2 <- sv_execute(v)
+  
+  expect_that(nrow(ds2), testthat::equals(10))  
+  expect_that(sv_rows(v), testthat::equals(10))  
+  expect_that(ncol(ds2), testthat::equals(2)) 
+  expect_that(names(ds2),testthat::equals(columns))
+  
+  expect_that(sum(ds2$Distance), testthat::equals(22265))
+  expect_that(min(ds2$Distance), testthat::equals(2027))
+  
+  expect_that(sum(ds2$TaxiIn), testthat::equals(35))
+  expect_that(max(ds2$TaxiIn), testthat::equals(4))
+  
+})
+
